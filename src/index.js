752aa049f0a7c79c2c9da047f98df3d5c4dc415b
musayann <musayann@octangrp.com>
2021-05-06 08:40:21 +0200

    Setup redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import { reducer } from './reducer/index';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true }))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-
