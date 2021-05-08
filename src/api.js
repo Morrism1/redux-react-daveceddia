6cd3e861b20283dbf4fbd983a924aec8bcacde70
Morrism1 <morrismontana0@gmail.com>
2021-05-08 15:04:47 +0200

    Refactor coursecreate function

--- src/api.js
+++ src/api.js
@@ -1,15 +1,17 @@
 import axios from "axios"
 
-export const courseCreate = (name, price) => {
-  return axios({
+export const courseCreate = async (name, price) => {
+  const data = await axios({
     method: 'POST',
     url: '/courses',
     data: { name: name, price: price }
   })
+
+  return data.data
 }
 
 export const getCourses = async () => {
-  let data = await axios.get('/courses').then(res => res)
+  const data = await axios.get('/courses').then(res => res)
 
   return data.data
 }
