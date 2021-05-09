d2d732f9556062cab3015f09e5cbcc0fe8591964
Morrism1 <morrismontana0@gmail.com>
2021-05-09 18:07:13 +0200

    Add Notfound page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+  return (
+    <div style={{ margin: 20 }}>
+      <h1>404 Not Found</h1>
+      <p>So sorry.</p>
+    </div>
+  )
+}
